0a4df6fe9d8695ca801522f462f366d1230c59fd
Iyadi Cyril Osita <Iyadicy@gmail.com>
2025-01-06 23:14:51 -0500

    initial commit

---
+++ src/test/java/com/see_why/api/spring_boot_demo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.see_why.api.spring_boot_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
