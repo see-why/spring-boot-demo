b0f867ff714facb29061a8e4299e23e0fab98c38
Iyadi Cyril Osita <Iyadicy@gmail.com>
2025-01-08 07:38:00 -0500

    customer model

---
+++ src/main/java/com/see_why/api/spring_boot_demo/Customer.java
@@ -0,0 +1,106 @@
+package com.see_why.api.spring_boot_demo;
+
+public class Customer {
+    private Integer Id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((age == null) ? 0 : age.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Customer other = (Customer) obj;
+        if (Id == null) {
+            if (other.Id != null)
+                return false;
+        } else if (!Id.equals(other.Id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (age == null) {
+            if (other.age != null)
+                return false;
+        } else if (!age.equals(other.age))
+            return false;
+        return true;
+    }
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Customer(Integer Id, Integer age, String email, String name) {
+        this.Id = Id;
+        this.age = age;
+        this.email = email;
+        this.name = name;
+    }
+
+    public Customer() {
+        
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Customer{");
+        sb.append("Id=").append(Id);
+        sb.append(", name=").append(name);
+        sb.append(", email=").append(email);
+        sb.append(", age=").append(age);
+        sb.append('}');
+        return sb.toString();
+    }
+}
