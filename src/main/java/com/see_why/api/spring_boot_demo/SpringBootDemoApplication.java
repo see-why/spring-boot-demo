d5dabac20748fcf78f384bbaaf8495e0a01ebed6
Iyadi Cyril Osita <Iyadicy@gmail.com>
2025-01-11 23:37:15 -0500

    add delete customer endpoint

--- src/main/java/com/see_why/api/spring_boot_demo/SpringBootDemoApplication.java
+++ src/main/java/com/see_why/api/spring_boot_demo/SpringBootDemoApplication.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,6 +55,11 @@ public class SpringBootDemoApplication {
 		customer.setEmail(request.email());
 		customer.setName(request.name());
 		customerRepository.save(customer);
+	}
+
+	@DeleteMapping("{customerId}")
+	public void deleteCustomer(@PathVariable("customerId") Integer id) {
+		customerRepository.deleteById(id);
 	}
 
     public CustomerRepository getCustomerRepository() {
