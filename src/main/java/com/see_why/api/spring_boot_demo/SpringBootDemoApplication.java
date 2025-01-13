f9759d793ea42a2cd0c8b9000afda6c70be05e8e
Iyadi Cyril Osita <Iyadicy@gmail.com>
2025-01-12 22:49:36 -0500

    put endpoint for customers

--- src/main/java/com/see_why/api/spring_boot_demo/SpringBootDemoApplication.java
+++ src/main/java/com/see_why/api/spring_boot_demo/SpringBootDemoApplication.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 
@@ -60,6 +62,20 @@ public class SpringBootDemoApplication {
 	@DeleteMapping("{customerId}")
 	public void deleteCustomer(@PathVariable("customerId") Integer id) {
 		customerRepository.deleteById(id);
+	}
+
+	@PutMapping("{customerId}")
+	public Customer updateCustomer(
+			@PathVariable("customerId") Integer id,
+			@RequestBody NewCustomerRequest request) {
+		Customer customer = customerRepository.findById(id)
+				.orElseThrow();
+		customer.setName(request.name());
+		customer.setEmail(request.email());
+		customer.setAge(request.age());
+		customerRepository.save(customer);
+
+		return customer;
 	}
 
     public CustomerRepository getCustomerRepository() {
